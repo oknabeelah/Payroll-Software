e500f1a4c4ff16e4cc94f6756cdef2255a252cfd
ok_nabeelah <110231886+oknabeelah@users.noreply.github.com>
2024-05-22 09:37:37 +0100

    added code fle

---
+++ CSProject-simple payroll software/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PayrollSoftware
+{
+    class Staff 
+    {
+        //contains info bout each staff
+        //contains CalculatePay() 
+    }
+
+    class Manager : Staff 
+    {
+        //inherit staffclass and ovverride CalculatePay()
+    }
+    class Admin : Staff 
+    {
+        //inherit staffclass and ovverride CalculatePay()
+    }
+    class FileReader 
+    {
+        //The FileReader class contains a simple method that reads from a .txt file and creates a list of Staff objects based on the contents in the.txt file.
+    }
+    class PaySlip 
+    {
+        /*The PaySlip class generates the pay slip of each employee in the company. In
+        addition, it also generates a summary of the details of staff who worked less than 10
+        hours in a month.*/
+
+    }
+    class Program 
+    {
+        public static void Main(String[] args)
+        {
+
+        }
+    }
+}
